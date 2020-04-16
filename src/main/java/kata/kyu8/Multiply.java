15fe9d103e6ec753b509d0f2c6e93a0cdd1218dc
Nataliia Postyka <63284405+NataliiaPostyka@users.noreply.github.com>
2020-04-16 15:40:27 -0700

    Update Multiply.java

--- src/main/java/kata/kyu8/Multiply.java
+++ src/main/java/kata/kyu8/Multiply.java
@@ -5,3 +5,12 @@ public class Multiply {
         return a * b;
     }
 }
+
+
+
+//Be Concise I - The Ternary Operator
+
+const describeAge = a => a<=12 ? "You're a(n) kid":
+a >= 13 && a <= 17?"You're a(n) teenager":
+a >=65?"You're a(n) elderly":
+"You're a(n) adult";
